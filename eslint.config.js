86591acf94e5ee69f238320e29a1bc906efc090c
jason081460 <jason081460@gmail.com>
2025-02-14 09:08:47 +0900

    fix(config): lint,ignore 파일 추가

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import react from 'eslint-plugin-react'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
+import prettier from 'prettier'
 
 export default tseslint.config(
 	{ ignores: ['dist'] },
@@ -19,6 +21,8 @@ export default tseslint.config(
 			'jsx-a11y': jsxA11y,
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
+			'no-relative-import-paths': noRelativeImportPaths,
+			'prettier': prettier
 		},
 		languageOptions: {
 			globals: globals.browser,
@@ -30,6 +34,23 @@ export default tseslint.config(
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
+			"prefer-destructuring": [
+				"error",
+				{
+					"VariableDeclarator": {
+						"array": false,
+						"object": true
+					},
+					"AssignmentExpression": {
+						"array": false,
+						"object": false
+					}
+				}
+			],
+			'no-relative-import-paths/no-relative-import-paths': [
+				'warn',
+				{ 'allowSameFolder': true, 'rootDir': 'src', 'prefix': '~' }
+			],
 			'react-refresh/only-export-components': [
 				'warn',
 				{ allowConstantExport: true },
@@ -38,11 +59,22 @@ export default tseslint.config(
 				'error',
 				'tab'
 			],
+			'semi': [
+				'error',
+				'never'
+			],
 			'react/jsx-indent': [2, 'tab'],
+			'react/jsx-filename-extension': ['error', { extensions: ['.js', '.tsx'] }],
 			'linebreak-style': ['error', 'windows'],
 			'react/jsx-wrap-multilines': 0,
 			'react/jsx-indent-props': [2, 'tab'],
 			'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+			'jsx-a11y/alt-text': [
+				'warn',
+				{
+					elements: ['img'],
+				},
+			],
 		},
 		settings: {
 			react: {
